c9d361d50d25be9fc9bda4e83160f27153e548d5
Camilla Kutzner <develop@camilla-kutzner.de>
2014-06-06 15:20:47 +0200

    made sample data, added script to insert data into anschreiben

---
+++ sample-data/insert_to_anschreiben.py
@@ -0,0 +1,20 @@
+# import methods
+from os.path import exists
+
+# prompt user for jobdescription and source, to be replaced with reading from other files later
+jobtitle = raw_input('Bitte gib den Jobtitel an: ')
+jobsrc = raw_input('Bitte gib eine Quelle ein: ')
+
+# get template file
+infile = open('anschreiben_strings.txt', 'r')
+template = infile.read()
+
+print "This is how your subject line now looks like:\n", template % (jobtitle, jobsrc)
+
+outfile = open('anschreiben_mit_strings.txt', 'w')
+outfile.write(str(template) % (jobtitle, jobsrc))
+
+outfile.close()
+infile.close()
+
+
