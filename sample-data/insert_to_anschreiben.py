988716f431205d076ef51c0fc2aa0171c4ffa05a
Camilla Kutzner <develop@camilla-kutzner.de>
2014-06-07 12:36:56 +0200

    added some variables to insertion script

--- sample-data/insert_to_anschreiben.py
+++ sample-data/insert_to_anschreiben.py
@@ -1,6 +1,15 @@
 # prompt user for jobdescription and source, to be replaced with reading from other files later
 jobtitle = raw_input('Bitte gib den Jobtitel an: ')
 jobsrc = raw_input('Bitte gib eine Quelle ein: ')
+company = raw_input('Bitte gib den Firmennamen ein: ')
+apforename =  raw_input('Bitte gib eine Quelle ein: ')
+aplastname =raw_input('Bitte gib eine Quelle ein: ')
+company_address = raw_input('Bitte gib eine Quelle ein: ')
+company_zipcode = raw_input('Bitte gib eine Quelle ein: ')
+company_city = raw_input('Bitte gib eine Quelle ein: ')
+ 
+
+#Anrede; case 1: sehr geehrte Frau, case2: sehr geehrter Herr, case3: sehr geehrte Damen und Herren
 
 # get template file
 infile = open('anschreiben_strings.txt', 'r')
