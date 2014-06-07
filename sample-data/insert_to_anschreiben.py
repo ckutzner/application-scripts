492997652a8d65bae03c7ee982a1d567c66280d7
Camilla Kutzner <develop@camilla-kutzner.de>
2014-06-07 12:44:08 +0200

    worked on variables in insertion script
    added appropriate prompts.

--- sample-data/insert_to_anschreiben.py
+++ sample-data/insert_to_anschreiben.py
@@ -1,14 +1,14 @@
 # prompt user for jobdescription and source, to be replaced with reading from other files later
 jobtitle = raw_input('Bitte gib den Jobtitel an: ')
 jobsrc = raw_input('Bitte gib eine Quelle ein: ')
-company = raw_input('Bitte gib den Firmennamen ein: ')
-apforename =  raw_input('Bitte gib eine Quelle ein: ')
-aplastname =raw_input('Bitte gib eine Quelle ein: ')
-company_address = raw_input('Bitte gib eine Quelle ein: ')
-company_zipcode = raw_input('Bitte gib eine Quelle ein: ')
-company_city = raw_input('Bitte gib eine Quelle ein: ')
+refstring = raw_input('Referenznummer:')
+company = raw_input('Firmenname: ')
+apforename =  raw_input('Vorname der_des Ansprechpartner_in (oder „Personalabteilung“): ')
+aplastname =raw_input('Nachname der_des Ansprechpartner_in: ')
+company_address = raw_input('Straße und Hausnummer: ')
+company_zipcode = raw_input('Postleitzahl: ')
+company_city = raw_input('Ort: ')
  
-
 #Anrede; case 1: sehr geehrte Frau, case2: sehr geehrter Herr, case3: sehr geehrte Damen und Herren
 
 # get template file
