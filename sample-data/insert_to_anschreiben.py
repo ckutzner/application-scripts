e4c36d4244f140390e13b5920a29566a0728e3da
Camilla Kutzner <develop@camilla-kutzner.de>
2014-06-08 14:15:23 +0200

    added encoding to insertion script, added string formatter to anschreiben_strings.txt

--- sample-data/insert_to_anschreiben.py
+++ sample-data/insert_to_anschreiben.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
 # prompt user for jobdescription and source, to be replaced with reading from other files later
 jobtitle = raw_input('Bitte gib den Jobtitel an: ')
 jobsrc = raw_input('Bitte gib eine Quelle ein: ')
-refstring = raw_input('Referenznummer:')
+refstring = '- Referenznummer' + raw_input('Referenznummer:') + ''
 company = raw_input('Firmenname: ')
-apforename =  raw_input('Vorname der_des Ansprechpartner_in (oder „Personalabteilung“): ')
+apforename =  raw_input('Vorname der_des Ansprechpartner_in (oder Personalabteilung): ')
 aplastname =raw_input('Nachname der_des Ansprechpartner_in: ')
 company_address = raw_input('Straße und Hausnummer: ')
 company_zipcode = raw_input('Postleitzahl: ')
@@ -11,11 +12,12 @@ company_city = raw_input('Ort: ')
  
 #Anrede; case 1: sehr geehrte Frau, case2: sehr geehrter Herr, case3: sehr geehrte Damen und Herren
 
-address = xyz
+#address = xyz
 
 # get template file
 infile = open('anschreiben_strings.txt', 'r')
 template = infile.read()
+print template #just for test purposes
 
 print "This is how your subject line now looks like:\n", template % (jobtitle, jobsrc)
 
