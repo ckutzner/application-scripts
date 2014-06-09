e74a3c85e8f95a2cf84a4473acb983c218d91ae6
ryuu on buechlein <thursa@gmx.de>
2014-06-09 14:26:20 +0200

    added 2nd script to read data from text file
    read_jobdata_2.py reads the job data and puts them in a dictionary.

---
+++ sample-data/read_jobdata_2.py
@@ -0,0 +1,28 @@
+# Initialize the data dictionary
+data_dictionary = {'Jobtitle:':'', 'Quelle:':'', 'Referenznummer:':'', 'Firma:':'', 'Ansprechperson:':'', 'Strasse:':'', 'PLZ:':'', 'Ort:':'', 'Telefon:':'', 'Email:':'', 'Anrede:':'', 'Jobdescription:':''}
+
+print "\n"
+
+# Read the jobdata file and fill the dictionary
+with open("jobdata.txt", mode='rt') as f:
+    for line in f:
+        tokens = line.split(None,1)
+        first_token = tokens[0]
+        remaining_tokens = tokens[1]
+
+        if remaining_tokens == '':
+            continue;
+
+        if first_token not in data_dictionary.keys():
+            print "WARNING: First token "+first_token+" not a key of the data_dictionary."
+            continue;
+
+        data_dictionary[first_token] = remaining_tokens
+        
+
+# Give warning if variables are missing
+for k in data_dictionary.keys():
+    if data_dictionary[k] == '':
+        print "WARNING: Variable "+k+" is an empty string."
+    else:
+        print "Variable "+k+" "+data_dictionary[k]
