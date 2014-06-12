c6d602b8744062a5e5f681f4d63b38a08c472a29
Camilla on buechlein <develop@camilla-kutzner.de>
2014-06-12 14:19:32 +0200

    added in l.18-21: last name isolation for opening
    splits name of responsible person into a list, returns only last item in this list for the letter opening.

--- sample-data/insert_to_anschreiben.py
+++ sample-data/insert_to_anschreiben.py
@@ -4,9 +4,7 @@ jobtitle = raw_input("Bitte gib den Jobtitel an: ")
 jobsrc = raw_input("Bitte gib eine Quelle ein: ")
 rawref = raw_input("Referenznummer: ") 
 company = raw_input("Firmenname: ")
-# separating forename from lastname is both counter-intuitive and cumbersome. find a more elegant solution, possibly by splitting the full name before last word?
-apforename =  raw_input("Vorname der_des Ansprechpartner_in (oder Personalabteilung): ")
-aplastname =raw_input("Nachname der_des Ansprechpartner_in: ")
+responsible =  raw_input("Ansprechpartner_in: ")
 company_address = raw_input("Straße und Hausnummer: ")
 company_zipcode = raw_input("Postleitzahl: ")
 company_city = raw_input("Ort: ")
@@ -16,6 +14,11 @@ if rawref == "" or rawref == " ":
 	refstring = ""
 else:
 	refstring = "- Referenznummer: " + rawref
+
+# turn name of responsible person into list, so that the last name can be used in the opening
+rpname = responsible.split(' ')
+addressee = rpname[-1]
+print "Der Nachname der verantwortlichen Person lautet", addressee
  
 #Anrede; case 1: sehr geehrte Frau, case2: sehr geehrter Herr, case3: sehr geehrte Damen und Herren
 address = ""
@@ -39,13 +42,13 @@ opening = ""
 if ad_input !="1" and ad_input!="2":
 	opening = address
 else:
-	opening = address + " " + aplastname
+	opening = address + " " + addressee
 
 # get template file
 with open('anschreiben_strings.txt', 'r') as infile:
 	template = infile.read()
 
-#print 'So sieht der Briefanfang jetzt aus:\n', template % (jobtitle, jobsrc, refstring, company, apforename, aplastname, company_address, company_zipcode, company_city, opening)
+#print 'So sieht der Briefanfang jetzt aus:\n', template % (jobtitle, jobsrc, refstring, company, responsible, company_address, company_zipcode, company_city, opening)
 
 with open('anschreiben_mit_strings.txt', 'w') as outfile:
-	outfile.write(str(template) % (jobtitle, jobsrc, refstring, company, apforename, aplastname, company_address, company_zipcode, company_city, opening))
+	outfile.write(str(template) % (jobtitle, jobsrc, refstring, company, responsible, company_address, company_zipcode, company_city, opening))
