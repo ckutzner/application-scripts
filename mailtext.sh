0618b43f1aa1432e268f621bc9ad67c5f6deb983
Camilla Kutzner <develop@camilla-kutzner.de>
2014-07-08 15:50:54 +0200

    fixed bug: mailtextedit.awk not found

--- mailtext.sh
+++ mailtext.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # This script generates mail.txt which, in turn, becomes the body text for the mail. Planned feature: strip unnecessary lines from text files, keep only opening of body of cover letter, but maybe add salutation (this could also be handled by mail-composition script).
 
-detex anschreiben.tex | awk -f mailtextedit.awk > mail.txt
+awkwhere=$(echo $(which mailtext.sh) | sed 's/mailtext.sh$//')
+detex anschreiben.tex | awk -f $awkwhere/mailtextedit.awk > mail.txt
 
 
 
