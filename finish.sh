2576e3a02838c76058d21034a0ae3ded0d4ff40c
Camilla Kutzner <develop@camilla-kutzner.de>
2014-08-17 19:16:48 +0200

    fixed missing space in l4

--- finish.sh
+++ finish.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Skript, das nicht mehr benötigte Files löscht; call from main directory. Achtung: danach können alle Bewerbungsskripte, die auf current_jobname und current_directory zugegriffen haben, nicht mehr ausgeführt werden!
 
-if [ -f current_jobdirectory]
+if [ -f current_jobdirectory ]
 then
 	jobdir=$(head -1 current_jobdirectory)
-	echo $jobdir
+#	echo $jobdir # for test purposes
 	rm ${jobdir}/*.aux 2> /dev/null
 	rm ${jobdir}/*.bak 2> /dev/null
 	rm ${jobdir}/*.log 2> /dev/null
