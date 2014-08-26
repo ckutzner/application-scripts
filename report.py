70a8565b2bd99e0f408249e2d45ad9e79a9c0137
Camilla Kutzner <develop@camilla-kutzner.de>
2014-08-26 16:18:29 +0200

    added some test material

--- report.py
+++ report.py
@@ -12,19 +12,27 @@ start = int(sys.argv[1]) # start month
 stop = int(sys.argv[2]) # stop month
 
 # make loopable range from months
-if stop >= start:
+if stop <= start:
 	stop += 12
 months = range(start, stop+1)
+print months #for test purpose
 
 filelist = []
-files_in_dir = os.listdir('.')
 
+# generate raw filelist
+raw_filelist = fnmatch.filter(os.listdir('.'), '[0-9][0-9][0-9][0-9]-[0-1][0-9]*[a-z].csv')
+print raw_filelist #for test purpose
+
+# filter raw filelist for occurrence of months
 for i in months:
 	if i > 12:
 		i -= 12
-		
+	for a in fnmatch.filter(raw_filelist, '[0-9][0-9][0-9][0-9]-*%d*.csv' % i):
+		filelist.append(a)
+	
+print '\n'.join(filelist) # for test purposes	
 
-# determine file
-for f in filelist:
-	with open(file, 'r') as d: 
+# read from final files
+#for f in filelist:
+#	with open(file, 'r') as d: 
 	
