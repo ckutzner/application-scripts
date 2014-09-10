e5d82f3717868f7cc16d9b965acbd6eb8a35a029
Camilla Kutzner <develop@camilla-kutzner.de>
2014-09-10 17:31:41 +0200

    1st trial version of LaTeX report writing
    still very clumsy. Need to figure out how to use the csv module together with unicode!
    
    Program now writes csv line to LaTeX.
    
    Desiderata:
     - process certain columns only (reference number, date of rejection etc. are not needed here)
     - concatenate fields: company name, street, zip code, city, but also email, phone
     - rearrange fields so they correspond to Jobcenter requirements
    For that, it may be necessary to assign the data to variables from the line_list.

--- report.py
+++ report.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Call this script from your main job application directory; enter start date and stop date as command line arguments 
+# Call this script from your main job application directory; enter start date and stop date (format m or mm, eg. 7 for July and 11 for November) as command line arguments 
 
 import sys
 import datetime
 import os
 import fnmatch
+import codecs
 
 # enter start and stop month
 start = int(sys.argv[1]) # start month
@@ -15,13 +16,11 @@ stop = int(sys.argv[2]) # stop month
 if stop <= start:
 	stop += 12
 months = range(start, stop+1)
-print months #for test purpose
 
 filelist = []
 
 # generate raw filelist
 raw_filelist = fnmatch.filter(os.listdir('.'), '[0-9][0-9][0-9][0-9]-[0-1][0-9]*[a-z].csv')
-print raw_filelist #for test purpose
 
 # filter raw filelist for occurrence of months
 for i in months:
@@ -30,16 +29,19 @@ for i in months:
 	for a in fnmatch.filter(raw_filelist, '[0-9][0-9][0-9][0-9]-*%d*.csv' % i):
 		filelist.append(a)
 	
-print '\n'.join(filelist) # for test purposes	
-
-# read from final files
-#for f in filelist:
-#	with open(f, 'r') as d: 
+# read from final files; certainly there is a more elegant way using the csv class and unicode, like described below there: https://docs.python.org/2/library/csv.html#examples . For now, I'll stick with this clumsy workaround.
+for f in filelist:
+	g = codecs.open(f, encoding='utf-8') #needed bccsv files contain unicode characters
+	for line in g:
+		sline= line.strip('\n"') # remove newline characters and quotes at the beginning of lines
+		line_list = sline.split('","') # make list, separated by commas surrounded by quotes
+		outline = ' & '.join(line_list) # join list back by LaTeX field separators
+		h = codecs.open('report_test.tex', encoding='utf-8', mode='a+')  # then write it to the outfile 
+		h.write(outline + '\\\ \hline\n')
+		h.close() # Finally, close the files 
+	g.close()
 # what to do: 
-# read line from csv
-# split lines along delimiter, assign to variables?
+# rewrite: assign to variables?
 # rearrange and join fields according to jobcenter requirements
-# learn how to do fields with line wraps in LaTeX tables
-# wrap fields with appropriate latex syntax
-# write to reportcontent.tex
-# bonus points for function that only produces entries in a given timedelta (e.g. from 2014-07-23 to 2014-10-22)
+# learn how to do fields with line wraps in LaTeX tables, either write formatting for it or incorporate it in writing program
+# bonus points for function that only produces entries in a given timedelta (e.g. from 2014-07-23 to 2014-10-22) instead of months
